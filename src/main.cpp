cd2618abef90f53a56f0b250d077174b622c1505
dimy20 <enzostrindade16@outlook.com>
2022-09-30 19:13:24 -0300

    cmake, main

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
+#include "Gb_emulator.h"
+#include "Gb_cartridge.h"
 int main(){
 	std::cout << "Gameboy Emulator!" << std::endl;
+	Gb_emulator gb;
+	gb.run("contra.gb");
 	return 0;
 };
