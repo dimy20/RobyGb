ddf86553c0a979918e2fbd54e28cc7b9a1158dc6
dimy20 <enzostrindade16@outlook.com>
2022-11-03 23:49:40 -0300

    emulator

--- include/Gb_emulator.h
+++ include/Gb_emulator.h
@@ -4,13 +4,25 @@
 #include "Memory.h"
 #include "Gb_cartridge.h"
 #include "Gb_core.h"
+#include "Gb_ppu.h"
+#include "Gb_timer.h"
+#include "Window.h"
 
+#include <unistd.h>
 class Gb_emulator{
 	public:
 		Gb_emulator();
 		void run(const char * filename);
 	private:
-		Mem_mu m_mmu;
+		void signal_interrupt(Gb_core::intrp i);
+		// updates system's timer, takes how many cycles have passed
+		// so we can synchronize it with the cpu.
+		void handle_timer(int cycles);
+	private:
+		Mem_mu m_memory;
 		Gb_core m_core;
+		Gb_ppu m_ppu;
+		Window m_window;
+		Gb_timer m_timer;
 };
 
--- src/Gb_emulator.cpp
+++ src/Gb_emulator.cpp
@@ -1,12 +1,39 @@
 #include "Gb_emulator.h"
 
-Gb_emulator::Gb_emulator(){};
+Gb_emulator::Gb_emulator(){
+	m_window.init();
+};
 
 void Gb_emulator::run(const char * filename){
 	Gb_cartridge cartridge(filename);
-	m_mmu.init(&cartridge);
-	m_core = Gb_core(&m_mmu);
+	m_timer.init(&m_memory);
+	m_memory.init(&cartridge, &m_timer);
+	m_core = Gb_core(&m_memory);
 	m_core.init();
-	m_core.emulate_cycles(4); // dummy for now
+	m_ppu.init(&m_memory, &m_window);
+	int max_cycles_per_frame = 69905;
+	int cycles = 0;
+	while(!m_window.should_close()){
+		cycles = 0;
+		while(cycles < max_cycles_per_frame){
+			cycles += m_core.execute_instruction();
+			m_ppu.update_graphics(cycles);
+			m_timer.update(cycles);
+			m_core.handle_interrupts();
+
+		};
+
+		m_window.draw();
+		m_window.swap_buffers();
+
+	};
+	std::cout << "Executed instructions ..." << std::endl;
 };
+
+void Gb_emulator::signal_interrupt(Gb_core::intrp i){
+	auto IF = m_memory.read(0xff0f);
+	IF |= (0x1 << i);
+	m_memory.write(0xff0f, IF);
+};
+
 
