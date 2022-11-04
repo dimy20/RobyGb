2f75401320f9c5a1f13bdaf1563ee8133493c3fc
dimy20 <enzostrindade16@outlook.com>
2022-11-03 23:31:21 -0300

    Memory

--- include/Memory.h
+++ include/Memory.h
@@ -1,7 +1,10 @@
 #pragma once
+#include <unordered_map>
+#include <functional>
 #include <cassert>
 #include "Gb_types.h"
 #include "Gb_cartridge.h"
+#include "Gb_timer.h"
 /* General Memory Map
  * The Gameboy has a 16 bit address bus, it is used for ROM, RAM and I/O.
  *
@@ -31,6 +34,10 @@
 #define IO_RANGE_SIZE 128
 #define HRAM_SIZE 127
 
+#define IF_ADDR 0xff0f // interrupt flag (request)
+#define IE_ADDR 0xffff // interrupt enable
+#define STAT_ADDR 0xff41
+
 struct Gb_memory{
 	BYTE rom_bank0[ROM_BANK_SIZE];
 	BYTE rom_bank1n[ROM_BANK_SIZE];
@@ -46,19 +53,61 @@ struct Gb_memory{
 	BYTE ie_register;
 };
 
+enum mem_region{
+	HRAM = 0,
+	WRAM,
+	ROM,
+	VRAM,
+	EXRAM,
+	OAM,
+	REGISTERS,
+	IE
+};
+
+class Gb_ppu;
 // Memory managment unit.
 class Mem_mu{
+	friend Gb_ppu;
+	friend Gb_timer;
+	public:
+	enum io_port{
+		P1 = 0Xff00,
+		SB = 0xff01,
+		IF = 0xff0f,
+		IE = 0xffff,
+		DIV = 0xff04,
+		TIMA = 0xff05,
+		TMA = 0xff06,
+		TAC = 0Xff07,
+		LY = 0xff44,
+		LCDC = 0xff40,
+		STAT = 0xff41,
+		BGP = 0xff47,
+		OBP0 = 0xff48,
+		OBP1 = 0xff49,
+		SCY = 0xff42,
+		SCX = 0xff43,
+		LYC = 0xff45
+	};
 	public:
 		Mem_mu();
 		BYTE read(WORD addr) const;
 		void write(WORD addr, BYTE value);
 		Gb_cartridge * cart() { return m_cart; };
-		void init(Gb_cartridge * cart);
+		void init(Gb_cartridge * cart, Gb_timer * timer);
+	protected:
+		Gb_memory * data(){ return &m_memory; };
 	private:
 		void write_wram_mirror(WORD addr, BYTE value);
 		void write_wram(WORD addr, BYTE value);
 		BYTE read_wram_mirror(WORD addr) const ;
+		void handle_io_ports(WORD addr, BYTE value);
+		void raise_restriction_hram_only(bool raise);
+		constexpr bool access_restricted(mem_region r) const {return ( m_access_restrictions >> r) & 0x1;};
 	private:
 		struct Gb_memory m_memory;
+
 		Gb_cartridge * m_cart = nullptr;
+		Gb_timer * m_timer = nullptr;
+		BYTE m_access_restrictions = 0;
 };
--- src/Memory.cpp
+++ src/Memory.cpp
@@ -3,8 +3,26 @@
 #include <cstring>
 
 Mem_mu::Mem_mu(){
-	// just for now
 	memset(&m_memory, 0, sizeof(struct Gb_memory));
+    write(0xff10, 0x80);
+    write(0xff11, 0xbf);
+    write(0xff12, 0xf3);
+    write(0xff14, 0xbf);
+    write(0xff16, 0x3f);
+    write(0xff19, 0xbf);
+    write(0xff1a, 0x7f);
+    write(0xff1b, 0xff);
+    write(0xff1c, 0x9f);
+    write(0xff1e, 0xbf);
+    write(0xff20, 0xFf);
+    write(0xff23, 0xbf);
+    write(0xff24, 0x77);
+    write(0xff25, 0xf3);
+    write(0xff26, 0xf1);
+    write(0xff40, 0x91);
+    write(0xff47, 0xfc);
+    write(0xff48, 0xff);
+    write(0xff49, 0xff);
 };
 
 BYTE Mem_mu::read(WORD addr) const {
@@ -19,10 +37,17 @@ BYTE Mem_mu::read(WORD addr) const {
 	if(addr >= 0xfea0 && addr <= 0xfeff) return 0x00;
 	if(addr >= 0xff00 && addr <= 0xff7f) return m_memory.io_registers[addr - 0xff00];
 	if(addr >= 0xff80 && addr <= 0xfffe) return m_memory.hram[addr - 0xff80];
-	std::cout << "read unimplemented " << std::endl;
+	if(addr == 0xffff){
+		return m_memory.ie_register;
+	};
+
+	std::cout << "read unimplemented " << std::hex << (unsigned int)addr << std::endl;
+	exit(1);
 };
 
 BYTE Mem_mu::read_wram_mirror(WORD addr) const {
+	if(access_restricted(mem_region::WRAM)) return 0xff;
+
 	WORD offset = addr - 0x2000;
 	if(offset >= 0xc000 && offset <= 0xcfff)
 		return m_memory.wram_bank0[offset - 0xc000];
@@ -32,6 +57,8 @@ BYTE Mem_mu::read_wram_mirror(WORD addr) const {
 }
 
 void Mem_mu::write_wram_mirror(WORD addr, BYTE value){
+	if(access_restricted(mem_region::WRAM)) return;
+
 	m_memory.wram_mirror[addr - 0xe000] = value;
 	// echo to work ram
 	WORD mem_offset = (addr - (0x2000));
@@ -44,6 +71,8 @@ void Mem_mu::write_wram_mirror(WORD addr, BYTE value){
 };
 
 void Mem_mu::write_wram(WORD addr, BYTE value){
+	//if(access_restricted(mem_region::WRAM)) return;
+
 	if(addr >= 0xc000 && addr <= 0xcfff)
 		m_memory.wram_bank0[addr - 0xc000] = value;
 	else if(addr >= 0xd000 && addr <= 0xdfff) 
@@ -58,26 +87,73 @@ void Mem_mu::write_wram(WORD addr, BYTE value){
 
 void Mem_mu::write(WORD addr, BYTE value){
 	// dont allow writes to ROM memory
-	if(addr <= 0x7fff) m_cart->mbc_intercept(addr, value);
+	if(addr <= 0x7fff){
+		if(access_restricted(mem_region::ROM)) return;
+		m_cart->mbc_intercept(addr, value);
+	}
 	else if(addr >= 0x8000 && addr <= 0x9fff){
+		//if(access_restricted(mem_region::VRAM)) return;
 		m_memory.vram[addr - 0x8000] = value;
 	}
-	else if(addr >= 0xa000 && addr <= 0xbfff) m_cart->write(addr, value);
+	else if(addr >= 0xa000 && addr <= 0xbfff){
+		if(access_restricted(mem_region::EXRAM)) return;
+		m_cart->write(addr, value);
+	}
 	else if(addr >= 0xfea0 && addr <= 0xfeff) return; //prohibited
 	else if(addr >= 0xe000 && addr <= 0xfdff) write_wram_mirror(addr, value);
 	else if(addr >= 0xc000 && addr <= 0xdfff) write_wram(addr, value);
-	else if(addr >= 0xff00 && addr <= 0xff7f){ // io registers
-		m_memory.io_registers[addr - 0xff00] = value;
-	}else if(addr >= 0xff80 && addr <= 0xfffe){ // high ram
+	else if(addr >= 0xff00 && addr <= 0xff7f) handle_io_ports(addr, value);
+	else if(addr >= 0xff80 && addr <= 0xfffe){ // high ram
 		m_memory.hram[addr - 0xff80] = value;
 	}else if(addr == 0xffff){
 		m_memory.ie_register = value;
 	}else{
-		std::cout << "Unimplemented yet" << std::endl;
+		std::cout << "write unimplemented " << std::hex << (unsigned int)addr << std::endl;
+		exit(1);
 	}
 };
 
-void Mem_mu::init(Gb_cartridge * cart){
+void Mem_mu::init(Gb_cartridge * cart, Gb_timer * timer){
 	m_cart = cart;
 	m_memory.io_registers[0x0044] = 0x90;
+	m_timer = timer;
+};
+
+void Mem_mu::handle_io_ports(WORD addr, BYTE value){
+	// attemp to write to current scanline, resets it.
+	if(addr == io_port::LY){
+		m_memory.io_registers[addr - 0xff00] = 0;
+		return;
+	}else if(addr == io_port::DIV){ // divider
+		m_memory.io_registers[addr - 0xff00] = 0;
+		return;
+	}else if(addr == io_port::TAC){ // timer controller
+		auto prev_freq = read(io_port::TAC);
+		m_memory.io_registers[addr - 0xff00] = value;
+
+		if((prev_freq & 0x3) != (value & 0x3)){
+			m_timer->update_frequency();
+		}
+		return;
+	}else if(addr == 0xff46){ // DMA transfer
+		// raise access restriction to hram only
+		m_access_restrictions = 0xfe;
+		// since this byte holds src/0x100, multiply to get original src addr
+		// DMA transfer takes 160 cycles and during this time the cpu can only
+		// acess HRAM.
+		WORD src_addr = value * 0x100;
+		for(int i = 0; i < 160; i++){
+			m_memory.sprite_attr_table[i] = read(src_addr + i);
+		};
+		return;
+	}else if(addr >= 0xff10 && addr <= 0xff26){
+		// not implemented (sound modes and waves patterns)
+		return;
+	}else if(addr == io_port::SB){
+		m_memory.io_registers[0xff01 - 0xff00] = value;
+		std::cout << value << " ";
+		return;
+	}else{
+		m_memory.io_registers[addr - 0xff00] = value;
+	}
 };
