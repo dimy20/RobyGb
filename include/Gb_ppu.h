ed6e234bc8be88495102e93eba7a05286a5f2fd6
dimy20 <enzostrindade16@outlook.com>
2022-11-03 23:12:16 -0300

    video oputput

--- include/Gb_ppu.h
+++ include/Gb_ppu.h
@@ -1,14 +1,53 @@
 #pragma once
 
 #include <iostream>
+#include <cassert>
+#include <vector>
 #include "Memory.h"
+#include "Window.h"
 
+#define VIEWPORT_WIDTH 160
+#define VIEWPORT_HEIGHT 144
 #define DRAW_SCANLINE_CYCLES 456 // it takes 456 cycles to draw a scanline
+
+// class to control lcdc and ldc stat
+class Lcd{
+	public:
+		enum mode{
+			HBLANK = 0,
+			VBLANKM = 1,
+			OAM = 2,
+			VRAM = 3,
+		};
+		enum intrp{
+			VBLANK = 0,
+			LCD,
+			TIMER,
+			SERIAL,
+			JOYPAD
+		};
+		Lcd() = default;
+		void init(Mem_mu * memory);
+
+		void set_mode(const int mode) const;
+		void ly_compare(BYTE line);
+		void fire_interrupt(intrp i) const;
+		WORD tile_data() const;
+		BYTE status() const;
+		BYTE control() const;
+
+		WORD bg_tilemap() const;
+		WORD wn_tilemap() const;
+		bool window_enabled() const;
+		bool enabled() const;
+	private:
+		Mem_mu * m_memory;
+};
 
 class Gb_ppu{
 	public:
-		Gb_ppu(Mem_mu * memory) : m_memory(memory) {};
-		void init();
+		Gb_ppu() = default;
+		void init(Mem_mu * memory, Window * window);
 		void update_graphics(int cycles);
 		enum intrp{
 			VBLANK = 0,
@@ -18,13 +57,28 @@ class Gb_ppu{
 			JOYPAD
 		};
 	private:
-		void draw_scanline();
+		void draw_scanline(int line);
 		bool enabled() const; // test 
-		void update_lcd_status();
-		void signal_interrupt(intrp i);
-		BYTE set_lcd_mode(BYTE status, int i);
+		void render_tiles(int scanline);
+		void render_sprites();
+		BYTE get_scanline() const{ return m_memory->read(0xff44); };
+		WORD pixel_find_tile(const WORD tilemap, BYTE pixel_x, BYTE pixel_y);
+		BYTE tile_assemble_pixel(WORD tile_addr, WORD x, WORD y);
+		BYTE palette_get_color(BYTE color_id);
+		void set_lcdc_mode(const int mode) const;
+
+
+		void tile_map_debug();
+
+		void make_map();
+		void build_tile(WORD tile_addr);
 	private:
-		Mem_mu * m_memory;
+		Window * m_window = nullptr; // emulator graphic output layer
+		Mem_mu * m_memory = nullptr;
 		// keep track of how many cycles are left to finished drawing current scanline.
 		int m_cycles_left = DRAW_SCANLINE_CYCLES;
+		std::vector<unsigned char> m_visible_screen;
+		std::vector<unsigned char> m_background;
+		int m_prev_line = -1;
+		Lcd m_lcd;
 };
--- src/Gb_ppu.cpp
+++ src/Gb_ppu.cpp
@@ -1,105 +1,216 @@
 #include "Gb_ppu.h"
 
-void Gb_ppu::init(){};
+void Lcd::init(Mem_mu * memory){
+	m_memory = memory;
+}
 
-void Gb_ppu::draw_scanline(){
-	std::cout << "to be implemented..." << std::endl;
-};
+BYTE Lcd::status() const { return m_memory->read(Mem_mu::io_port::STAT); };
+BYTE Lcd::control() const { return m_memory->read(Mem_mu::io_port::LCDC); };
+bool Lcd::enabled() const{ return status() >> 7;};
+bool Lcd::window_enabled() const{ return control() & 0x20; };
+WORD Lcd::bg_tilemap() const{ return control() & 0x08 ? 0x9c00 : 0x9800; };
+WORD Lcd::wn_tilemap() const{ return control() & 0x40 ? 0x9c00 : 0x9800; };
 
-void Gb_ppu::update_graphics(int elapsed_cycles){
-	update_lcd_status();
-
-	m_cycles_left -= elapsed_cycles;
-
-	if(!enabled() || m_cycles_left > 0) return;
-
-	auto memory = m_memory->data();
-	auto curr_line = ++memory->io_registers[LY_ADDR - 0xff00];
-
-	m_cycles_left = DRAW_SCANLINE_CYCLES;
-	// vertical blank
-	if(curr_line >= 144){
-		signal_interrupt(intrp::VBLANK);
-	}else if(curr_line > 153){
-		memory->io_registers[LY_ADDR - 0xff00] = 0;
-	}else if(curr_line >= 0 && curr_line <= 143){
-		draw_scanline();
-	};
-};
-
-bool Gb_ppu::enabled() const{ 
-	auto lcdc_reg = m_memory->read(LCDC); 
-	return (lcdc_reg >> 7) & 0x1;
-};
-
-void Gb_ppu::update_lcd_status(){
-	BYTE status = m_memory->read(STAT_ADDR); 
-	/*If lcd is disabled, reset scanline and put mode 1 (vblank) back.*/
-	if(!enabled()){
-		m_cycles_left = DRAW_SCANLINE_CYCLES;
-
-		// set current scanline number back to 0
-		auto memory = m_memory->data();
-		memory->io_registers[LY_ADDR - 0xff00] = 0;
-
-		status = set_lcd_mode(status, 1);
-		m_memory->write(STAT_ADDR, status);
-
-		return;
-	};
-
-	BYTE prev_mode = status & 0x3;
-
-	int mode2_lower_bound = DRAW_SCANLINE_CYCLES - 80; // mode 2 duration is 80 dots.
-	// mode 3 duration is 168 - 292 dots, depending on sprite count
-	// TODO: update this value dynamically depending on sprite count.
-	int mode3_lower_bound = DRAW_SCANLINE_CYCLES - 168; 
-
-	auto curr_line = m_memory->read(LY_ADDR);
-	bool req_intrp = false;
-
-	// fire interrupt only when state changes to mode 1, 0 or 2.
-	if(curr_line >= 144){
-		// TODO: raise all restriction
-		status = set_lcd_mode(status, 1);
-		req_intrp = (status >> 4) & 0x1;
-	}else if(m_cycles_left >= mode2_lower_bound){
-		// TODO: raise all restriction but oam
-		status = set_lcd_mode(status, 2);
-		req_intrp = (status >> 5) & 0x1;
-	}else if(m_cycles_left >= mode3_lower_bound){
-		//TODO : oam restrict and all of vram
-		status = set_lcd_mode(status, 3);
-	}else{
-		// TODO: raise all restriction
-		status = set_lcd_mode(status, 0);
-		req_intrp = (status >> 3) & 0x1;
-	}
-
-	if(req_intrp && (prev_mode != (status & 0x3)))
-		signal_interrupt(intrp::LCD);
-
-	if(curr_line == m_memory->read(0xff45)){
-		status |= 0x1 << 2; // coincidence bit
-		if((status >> 6) & 0x1)
-			signal_interrupt(intrp::LCD);
-	}else{
-		status &= ~(0x1 << 2);
-	}
-
-	m_memory->write(STAT_ADDR, status);
-};
-
-void Gb_ppu::signal_interrupt(intrp i){
+void Lcd::fire_interrupt(intrp i) const {
 	auto intrp_flag = m_memory->read(IF_ADDR);
 	intrp_flag |= (0x1 << i);
 	m_memory->write(IF_ADDR, intrp_flag);
 };
 
+void Lcd::set_mode(const int mode) const {
+	assert(mode >=0 && mode <= 3);
+	BYTE stat = status();
 
-BYTE Gb_ppu::set_lcd_mode(BYTE status, int mode){
-	assert(mode <= 3);
-	status &= 0xfc;
-	status |= mode;
-	return status;
+	stat &= 0xfc;
+	stat |= mode;
+
+	m_memory->write(Mem_mu::io_port::STAT, stat);
+
+	if(mode == 3) return;
+
+	// fire interrupt if sources for mode 2, 0 or 1 are set.
+	if((stat & 0x20) || (stat & 0x10) || (stat & 0x08)){
+		fire_interrupt(intrp::LCD);
+	}
 };
+
+void Lcd::ly_compare(BYTE line){
+	BYTE lyc = m_memory->read(Mem_mu::io_port::LYC);
+	BYTE stat = status();
+
+	if(line == lyc){
+		stat |= 0x04;; // set coincidence
+		if(stat & 0x40){
+			fire_interrupt(intrp::LCD);
+		}
+	}
+};
+
+WORD Lcd::tile_data() const{
+	return ((control() >> 4) & 1) ? 0x8000 : 0x9000;
+};
+
+void Gb_ppu::init(Mem_mu * memory, Window * window){
+	m_memory = memory;
+	m_window = window;
+	m_visible_screen = std::vector<unsigned char>(VIEWPORT_WIDTH * VIEWPORT_HEIGHT * 3, 0xff);
+
+	m_background = std::vector<unsigned char>(256 * 256 * 3);
+	m_lcd.init(memory);
+};
+
+void Gb_ppu::draw_scanline(int line){
+	render_tiles(line);
+		/*
+		return;
+	BYTE control = m_memory->read(Mem_mu::io_port::LCDC);
+	if(control & 0x1){
+
+	};
+	//if((control >> 1) & 0x1)
+		render_sprites();
+		*/
+};
+
+void Gb_ppu::update_graphics(int elapsed_cycles){
+
+	// takes 70224 clicks to render a frame
+	// takes 456 clicks to render an entire line
+	int current_frame = elapsed_cycles % 70224; // upper bound because it takes less
+	int line = current_frame / 456; // current line
+
+	int clicks = current_frame % 456;
+
+	// edge case
+	if(!m_lcd.enabled()){
+		// start drawing from line 0 again?
+		// set mode to vblank (1)
+		// return;
+	};
+
+	// direct access, writes to LY resets value back to 0.
+	if(line != m_prev_line)
+		m_memory->m_memory.io_registers[Mem_mu::io_port::LY - 0xff00] = line;
+
+	//std::cout << line  << std::endl;
+	if(line < 144){
+		if(clicks <= 80){
+			m_lcd.set_mode(Lcd::mode::OAM);
+		}else if(clicks <= 289){
+			m_lcd.set_mode(Lcd::mode::VRAM);
+		}else if(clicks < 456){
+			m_lcd.set_mode(Lcd::mode::HBLANK);
+		};
+
+		m_lcd.ly_compare(line);
+	}
+
+	if(line != m_prev_line && line < 144){
+		draw_scanline(line);
+		m_prev_line = line;
+	};
+
+	// Render frame
+	if(line == 144){
+		m_lcd.set_mode(Lcd::mode::VBLANKM);
+		// send texture to opengl
+		m_window->ppu_recv(m_visible_screen);
+	}
+
+};
+
+void Gb_ppu::render_tiles(int scanline){
+	auto scy = m_memory->read(Mem_mu::io_port::SCY);
+	auto scx = m_memory->read(Mem_mu::io_port::SCX);
+	WORD tile_map;
+	// Window or bg?
+	if(m_lcd.window_enabled()){
+		// implement window stuff
+	}else{
+		// select map ?
+		tile_map = m_lcd.bg_tilemap();
+	}
+
+	// draw each pixel of the viewport for the current scanline.
+	for(BYTE pixel_x = 0; pixel_x < VIEWPORT_WIDTH; pixel_x++){
+		// pixels x and y coordinates in the tile map.
+		// Wraps around if exceeds edges.
+		WORD curr_x = (pixel_x + scx) % 256;
+		WORD curr_y = (scanline + scy) % 256;
+		// find the tile this pixel belongs to
+		WORD tile_addr = pixel_find_tile(tile_map, curr_x, curr_y);
+		// build the pixel
+		BYTE pixel_value = tile_assemble_pixel(tile_addr, curr_x % 8, curr_y % 8); // get a pixel
+		// get the final color from palette
+		BYTE color = palette_get_color(pixel_value);
+		BYTE red, green, blue;
+
+		switch (color){
+			case 0: red = 0xff; green = 0xff; blue = 0xff; break; // white
+			case 1: red = 0xcc; green = 0xcc; blue = 0xcc; break; // light grey
+			case 2: red = 0x77; green = 0x77; blue = 0x77; break; // dark grey
+			case 3: red = green = blue = 0; break; // black
+		}
+		auto offset = ((scanline * VIEWPORT_WIDTH) + pixel_x) * 3;
+		m_visible_screen[offset] = red;
+		m_visible_screen[offset + 1] = green;
+		m_visible_screen[offset + 2] = blue;
+	};
+
+};
+
+
+BYTE Gb_ppu::tile_assemble_pixel(WORD tile_addr, WORD x, WORD y){
+	BYTE offset = (y * 2); // offset within 16 bytes of this tile
+
+	BYTE b1 = m_memory->read(tile_addr + offset); // constains low bits
+	BYTE b2 = m_memory->read(tile_addr + offset + 1); // contains high bits
+
+	BYTE bit = 7 - x; // x coord in this line
+
+	BYTE lo = (b1 >> bit) & 1;
+	BYTE hi = ((b2 >> bit) & 1) << 1;
+	
+	return lo | hi;
+};
+
+BYTE Gb_ppu::palette_get_color(BYTE color_index){
+	BYTE lo_bit = 0, hi_bit = 0;
+
+	lo_bit = (color_index * 2);
+	hi_bit = (color_index * 2) + 1;
+
+	BYTE palette = m_memory->read(Mem_mu::io_port::BGP);
+
+	BYTE color = ((palette >> lo_bit) & 1) | (((palette >> hi_bit) & 1) << 1);
+
+	return color;
+};
+
+// find a the tile addr for a given pixel (x, y) in the tilemap given.
+WORD Gb_ppu::pixel_find_tile(const WORD tilemap, BYTE pixel_x, BYTE pixel_y){
+	// location of tile number in map
+	BYTE tile_num_x = pixel_x / 8;
+	BYTE tile_num_y = pixel_y / 8;
+
+	WORD tile_num_addr = tilemap + ((tile_num_y * 32) + tile_num_x);
+	// get tile num
+	BYTE tile_num = m_memory->read(tile_num_addr);
+
+	WORD tile_data = m_lcd.tile_data();
+	bool signed_adressing = tile_data == 0x9000;
+
+	// offset of tile in tile data region
+	WORD offset;
+	if(!signed_adressing){
+		offset = tile_data + (tile_num * 16);
+	}else{
+		offset = tile_data + (static_cast<SIGNED_BYTE>(tile_num) * 16);
+	}
+
+	return offset;
+};
+
+// not implemented yet
+void Gb_ppu::render_sprites(){};
+
