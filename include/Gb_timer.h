97c712a3172b82a45098b02ced0e4d8dbea5f39b
dimy20 <enzostrindade16@outlook.com>
2022-11-10 21:53:26 -0300

    Changed timer implementetion to use the 16-bit internal counter

--- include/Gb_timer.h
+++ include/Gb_timer.h
@@ -1,22 +1,48 @@
 #pragma once
 
 #define CLOCK_SPEED 4194304
+#include "Gb_types.h"
+#include "Gb_interrupts.h"
 
 class Mem_mu;
 class Gb_timer{
 	friend Mem_mu;
 	public:
 		Gb_timer() = default;
-		void init(Mem_mu * memory);
-		void update(int cycles);
+		void init(Gb_interrupts * intrs);
+		void cycle(int cycles);
+
 	private:
-		void send_interrupt(int i);
-		bool enabled(); // timer enbaled?
-		void update_divider(int cycles); // updates devider register
-		void update_frequency(); // updates timer frequency acording to TAC
+		void tick(int delta);
+		constexpr BYTE TMA () const { return m_TMA; };
+		constexpr BYTE DIV() const { return m_counter >> 8; };
+		constexpr BYTE TIMA() const { return m_TIMA; };
+		constexpr BYTE TAC() const { return m_TAC; };
+
+		constexpr BYTE tima_enabled() const { return m_TAC & 4; };
+		constexpr BYTE tima_speed() const { return m_TAC & 3; };
+
+		constexpr void set_TMA(BYTE value) {  m_TMA = value; };
+		constexpr void set_TIMA(BYTE value) { m_TIMA = value; };
+
+		void set_TAC(BYTE value);
+		void set_DIV(void);
+
+		void check_overflow(unsigned short tima);
 	private:
-		int m_timer_counter;
-		int m_divider_counter;
-		int m_frequency;
-		Mem_mu * m_memory;
+		Gb_interrupts * m_intrs;
+
+		BYTE m_TIMA = 0; // main timer
+		BYTE m_TAC = 0; // control
+		BYTE m_TMA = 0; // modulo
+
+		int m_prev_cycles = 0;
+
+		const unsigned int m_speeds[4] = {1024, 16, 64, 256}; // TIMA clock speeds.
+		// Timer internal counter; MSB is the div register;
+		unsigned short m_counter = 0xabcc;
+		// variables to handle overflow delayed by 4 clocks.
+		int m_tima_delay = 0;
+		bool m_overflow = false;
 };
+
--- src/Gb_timer.cpp
+++ src/Gb_timer.cpp
@@ -1,64 +1,117 @@
 #include "Gb_timer.h"
 #include "Memory.h"
 
-void Gb_timer::init(Mem_mu * memory){
-	m_memory = memory;
-	m_frequency = 4096; // default timer frequency
-	m_divider_counter = 256;
-	m_timer_counter = CLOCK_SPEED / m_frequency;
+void Gb_timer::init(Gb_interrupts * intrs){ m_intrs = intrs; };
+
+void Gb_timer::tick(int delta){
+	while(delta--){
+		// handle delayed overflow.
+		if(m_overflow){
+			m_tima_delay--;
+			if(m_tima_delay <= 0){
+				m_intrs->request(2);
+				m_TIMA = m_TMA; // set back to modulo
+				m_overflow = false;
+			}
+		}
+
+		/*
+		 * Counter increments once every cycle (4 clocks)
+		 * 1M elapsed -> += 4 ticks
+		 * After 256 T-clocks, the internal counter wil overflow and DIV register will increment.
+		 * DIV register = MSB of the internal counter.
+		 */
+
+		auto prev_counter = m_counter;
+		m_counter++;
+
+		/*
+		 * TIMA is increased by selecting either bit 3, 5, 7 or 9 from the internal
+		 * counter, which bit is selected depends on the selected speed for the clock
+		 * located at TAC bits 1-0.
+		 * Bit 3 -> speed 1
+		 * Bit 5 -> speed 2
+		 * Bit 7 -> speed 3
+		 * Bit 9 -> speed 0
+		 * It then ands this bit with the TAC enable bit.
+		 * As soon as this bit goes from 1 to 0 TIMA is incremented.
+		 */
+
+		int clock_speed = tima_speed(); // TAC bits 1-0
+
+		int prev_state = prev_counter & (m_speeds[clock_speed] >> 1);
+		int curr_state = m_counter & (m_speeds[clock_speed] >> 1);
+
+		if(!tima_enabled()) return;
+
+		// bit goes from 1 -> 0
+		if(prev_state && curr_state == 0){
+			check_overflow(++m_TIMA);
+		}
+
+	};
 };
 
-void Gb_timer::update(int cycles){
-	update_divider(cycles);
 
-	if(!enabled()) return;
+void Gb_timer::set_TAC(BYTE value){
+	bool glitch = false;
+	bool old_enabled = tima_enabled();
+	bool old_speed = tima_speed();
+	bool new_enabled = value & 4;
+	bool new_speed = value & 3;
 
-	m_timer_counter -= cycles;
-
-	if(m_timer_counter <= 0){
-		BYTE timer_counter = m_memory->read(Mem_mu::io_port::TIMA);
-
-		if(timer_counter == 0xff){
-			// reset
-			BYTE timer_modulo = m_memory->read(Mem_mu::io_port::TMA);
-			m_memory->write(Mem_mu::io_port::TIMA, timer_modulo);
-			// interrupt
-			send_interrupt(2);
+	/* Changing the value in the TAC register can have some unexpected increments in TIMA.*/
+	if(!old_enabled){
+		glitch = false;
+	}else{
+		/* Glitch 1
+		 * If TAC's enabled bit goes from high to low and if the old corresponding speed bit
+		 * is set in the internal counter, then TIMA will increment. */
+		if(!new_enabled){ // enabled bit : 1 -> 0
+			glitch = m_counter & (m_speeds[old_speed] >> 1);
 		}else{
-			m_memory->write(Mem_mu::io_port::TIMA, timer_counter + 1);
+			/* Glitch 2
+			 * When changing TAC value, if the old speed bit was 0 and the new one is 1,
+			 * and the new TAC value's enabled bit is 1, TIMA will increment. */
+			glitch = !(m_counter & (m_speeds[old_speed] >> 1)) && (m_counter & (m_speeds[new_speed] >> 1));
 		}
 	};
+
+	if(glitch)
+		check_overflow(++m_TIMA);
+	m_TAC = value;
 };
 
-void Gb_timer::send_interrupt(int i){
-	auto IF = m_memory->read(0xff0f);
-	IF |= (0x1 << i);
-	m_memory->write(0xff0f, IF);
-};
+void Gb_timer::set_DIV(){
+	/* Glitch 3
+	 * Any attempt to write to the DIV register resets it back to 0, resetting the
+	 * internal counter as well since DIV is just the MSB of the internal counter.
+	 * This causes the current clock speed bit (3, 5, 7, or 9) to go from 1 to 0,
+	 * so it triggers a TIMA increment.
+	 * */
+	int old_bit = m_counter & (m_speeds[tima_speed()] >> 1);
+	m_counter = 0; // write to div resets it to 0.
 
-
-bool Gb_timer::enabled(){
-	BYTE timer_control = m_memory->read(0xff07);
-	return (timer_control >> 2);
-};
-
-void Gb_timer::update_frequency(){
-	BYTE freq = m_memory->read(0xff07) & 0x3;
-	switch (freq){
-		case 0: m_frequency = 4096;   break ;
-		case 1: m_frequency = 262144; break ;
-		case 2: m_frequency = 65536;  break ;
-		case 3: m_frequency = 16382;  break ;
+	if(old_bit){ // 1 -> 0
+		check_overflow(++m_TIMA);
 	}
-	m_timer_counter = CLOCK_SPEED / m_frequency;
 };
 
-void Gb_timer::update_divider(int cycles){
-	auto divider_reg = m_memory->m_memory.io_registers[0xff04 - 0xff00];
-	m_divider_counter -= cycles;
-	if(m_divider_counter <= 0){
-		divider_reg = divider_reg == 0xff ? 0 : divider_reg + 1;
-		m_divider_counter = 256;
-		m_memory->m_memory.io_registers[0xff04 - 0xff00] = divider_reg;
-	};
+void Gb_timer::check_overflow(unsigned short tima){
+	if((tima & 0xff) == 0){ // overflow
+		// overflow handling is delayed by 4 clocks.
+		m_TIMA = 0; // during delay tima holds the value 0x00.
+		m_tima_delay = 4;
+		m_overflow = true;
+	}
+};
+
+void Gb_timer::cycle(int cycles){
+	if(cycles < m_prev_cycles) // we've entered next frame.
+		m_prev_cycles = 0;
+
+	int delta = cycles - m_prev_cycles; // elapsed clocks since last execution
+	m_prev_cycles = cycles;
+
+	tick(delta);
 };
